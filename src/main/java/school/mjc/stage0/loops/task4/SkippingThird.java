78f1d88ea05532afa99b1702bc64b8232c782859
MrFun01 <hprobook48@gmail.com>
2023-04-10 14:02:39 +0500

    Finished task 4 loops on the module 4

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int c=3;
+        for (int i = 1; i <= lastPrinted; i++) {
+            if (i==c){
+                c+=3;
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
